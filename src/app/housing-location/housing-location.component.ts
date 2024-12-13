0023ced4bdf618e3a0c517fc322a62ed9dfad1d4
Taca Karawang <taca.karawang911@gmail.com>
2024-12-13 06:55:54 -0800

    first commit

---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [],
+  template: `
+    <section>
+      aaa
+    </section>
+  `,
+  styleUrl: './housing-location.component.css',
+})
+export class HousingLocationComponent {}
